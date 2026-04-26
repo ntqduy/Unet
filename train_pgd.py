a6ff39bc381e5ad1c679b9372732ed499836f0ab
ntqduy <23127181@student.hcmus.edu.vn>
2026-04-26 10:56:22 +0700

    Update

--- train_pgd.py
+++ train_pgd.py
@@ -277,7 +277,7 @@ parser.add_argument(
 parser.add_argument("--seed", type=int, default=1337)
 parser.add_argument("--deterministic", type=int, default=1)
 parser.add_argument("--gpu", type=str, default="0")
-parser.add_argument("--num_workers", type=int, default=2)
+parser.add_argument("--num_workers", type=int, default=4)
 parser.add_argument("--save_visualizations", type=int, default=1)
 parser.add_argument("--vis_num_samples", type=int, default=8)
 parser.add_argument("--final_eval_splits", nargs="*", default=["train", "val", "test"])
